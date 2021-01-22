58e039ddbc16ac41f429d2a4eeda013befb52283
Erika <erikagonza2013@gmail.com>
2021-01-22 17:25:18 -0500

    Actualizacion de proyecto

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import validator from './validator.js';
 let btnEnviar= document.getElementById("buttonOne")
 
 btnEnviar.addEventListener('click', ()=>{
+    document.getElementById("new").innerHTML = "";
+    document.getElementById("newTwo").innerHTML = "";
     let name = document.getElementById("nameText").value;
     let number = document.getElementById("numberCardV").value;
+    let maskify = validator.maskify(number);
+    let isValid = validator.isValid(number);
+
 
     // Si ingresan un valor que no sea un numero y si el espacio esta vacio, saldra una alerta
     if (number === "" || (isNaN(number) === true) ){
@@ -18,35 +23,35 @@ btnEnviar.addEventListener('click', ()=>{
         return;
         }
 
-    // Si la tarjeta es valida, se visualizara el "secondPage"... Si la tarjeta es inválida se visualizara el "thirdBody"
-       /* if (number ===  ){
-            document.getElementById("secondPage").style.display = "block";   
+    // Si la tarjeta es valida, se visualizara el "secondPage"... Si la tarjeta es inválida se visualizara el "thirdBody", por ahora esta con Name
+       if (isValid === true){
+        document.getElementById("secondPage").style.display = "block";
+        document.getElementById("new").innerHTML += `<p id= "greentingNumber" class="secondLetters">${maskify}</p>`
+        document.getElementById("new").innerHTML += `<p id= "greentingName" class="secondLetters">${name+ " tu tarjeta es válida"}</p>`
+
              } else {
-            document.getElementById("thirdPage").style.display = "block";   
- 
-             } */
+            document.getElementById("thirdPage").style.display = "block";
+            document.getElementById("newTwo").innerHTML += `<p id= "greentingNumberTwo" class="thirdLetters">${maskify}</p>`
+            document.getElementById("newTwo").innerHTML += `<p id= "greentingNameTwo" class="thirdLetters">${name+ " tu tarjeta es inválida"}</p>`
+            } 
 
-        document.getElementById("secondPage").style.display = "block"; 
 
-    // Espacios vacios donde ira el número que ingresen y nombre del "secondPage"
-        document.getElementById("secondBody").innerHTML += `<p id= "greentingNumber" class="secondLetters">${number+ " Ocultar digitos"}</p>`
-        document.getElementById("secondBody").innerHTML += `<p id= "greentingName" class="secondLetters">${name+ " tu tarjeta es válida"}</p>`
-     
-    // Espacios vacios donde ira el número que ingresen y nombre del "thirdPage"
-        document.getElementById("thirdBody").innerHTML += `<p id= "greentingNumberTwo" class="thirdLetters">${number+ " Ocultar digitos"}</p>`
-        document.getElementById("thirdBody").innerHTML += `<p id= "greentingNameTwo" class="thirdLetters">${name+ " tu tarjeta es inválida"}</p>`
-        
+})
 let btnHouse= document.getElementById("house")
 
 btnHouse.addEventListener('click', ()=>{
 
+
     // Al presionar el boton "house" se devolvera la pagina de inicio
     document.getElementById("secondPage").style.display = "none";
     document.getElementById("firstPage").style.display = "block";
-    
-let btnHome= document.getElementById("home")
 
-btnHome.addEventListener('click', ()=>{
+
+})
+
+let btnHouseTwo= document.getElementById("houseTwo")
+
+btnHouseTwo.addEventListener('click', ()=>{
 
     // Al presionar el boton "home" se devolvera la pagina de inicio
     document.getElementById("thirdPage").style.display = "none";
@@ -54,11 +59,6 @@ btnHome.addEventListener('click', ()=>{
 
 
 })
-
-})
-
-})
- 
 
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,43 @@
 const validator = {
-  
-  
+
+    isValid : function(cardV) {
+        let doubleUp = false;
+        let sum = 0;
+        let curDigit = "";
+
+        for (let i = cardV.length - 1; i >= 0; i--) {
+
+           curDigit = parseInt(cardV.charAt(i));
+
+        if (doubleUp) {
+
+            if ((curDigit*2) > 9) {
+                sum +=(curDigit*2)-9;
+            } else {
+                sum += curDigit*2;
+            }
+        } else {
+            sum += curDigit;
+        }
+        doubleUp =!doubleUp;
+             }
+             return (sum % 10) == 0  ? true : false;
+    },
+
+        maskify: function(card){
+        let result = ""
+        for(let i=0; i < card.length; i++){
+
+        if (i <= card.length - 5) {
+            result += "#"
+
+        } else {
+            result += card[i]
+        }
+        }
+        return result
+
+    }
 
 };
 
