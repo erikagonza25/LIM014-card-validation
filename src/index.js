ad8328e5f1f7c7a0d0e11fc21ec47f81ad5e28b9
Erika <erika.gonzalez@enviame.io>
2024-04-30 16:32:23 -0500

    validation-card: Mejora en diseño

--- src/index.js
+++ src/index.js
@@ -1,63 +1,81 @@
 import validator from './validator.js';
-const btnEnviar= document.getElementById("buttonOne")
 
-btnEnviar.addEventListener('click', ()=>{
-    document.getElementById("cardInformation").innerHTML = "";
-    document.getElementById("secondInformation").innerHTML = "";
-    let name = document.getElementById("nameCard").value;
-    let number = document.getElementById("numberCardV").value;
+const btnEnviar = document.getElementById("buttonOne");
+const numberInput = document.getElementById("numberCardV");
+const nameInput = document.getElementById("nameCard");
+
+const numeroTarjeta = document.querySelector('#tarjeta .numero')
+const nombreTarjeta = document.querySelector('#tarjeta .nombre')
+const logoMarca = document.querySelector('#logo-marca')
+
+function checkInputs() {
+    const numberValue = numberInput.value.trim();
+    const nameValue = nameInput.value.trim();
+    
+    if (numberValue !== "" && nameValue !== "") {
+        btnEnviar.removeAttribute("disabled");
+    } else {
+        btnEnviar.setAttribute("disabled", "disabled");
+    }
+}
+
+numberInput.addEventListener("input", (e) => {
+    checkInputs
+    const valorInput = e.target.value;
+    numeroTarjeta.textContent = valorInput;
+    if(valorInput == ''){
+		numeroTarjeta.textContent = '#### #### #### ####';
+        logoMarca.innerHTML = '';
+	}
+    if(valorInput[0] == 4){
+		logoMarca.innerHTML = '';
+		const imagen = document.createElement('img');
+		imagen.src = './images/Project/visa.png';
+		logoMarca.appendChild(imagen);
+	} else if(valorInput[0] == 5){
+		logoMarca.innerHTML = '';
+		const imagen = document.createElement('img');
+		imagen.src = './images/Project/master.png';
+		logoMarca.appendChild(imagen);
+	}
+    else if(valorInput[0] == 3){
+		logoMarca.innerHTML = '';
+		const imagen = document.createElement('img');
+		imagen.src = './images/Project/dinners.png';
+		logoMarca.appendChild(imagen);
+	}
+});
+
+nameInput.addEventListener("input", (e) => {
+    checkInputs
+    const valorInput = e.target.value;
+    nombreTarjeta.textContent = valorInput;
+    if(valorInput == ''){
+		nombreTarjeta.textContent = 'John Doe';
+	}
+});
+
+nameInput.addEventListener("input", checkInputs);
+
+btnEnviar.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    let name = nameInput.value.trim();
+    let number = numberInput.value.trim();
     let maskify = validator.maskify(number);
     let isValid = validator.isValid(number);
 
-
-    // Si ingresan un valor que no sea un numero y si el espacio esta vacio, saldra una alerta
-    if (number === "" || (isNaN(number) === true) ){
-        alert("Ingrese un número de tarjeta válido");
-        return;
-        }
-    // Si el campo del nombre esta vacio saldra una alerta
-    if(name !==""){
-        document.getElementById("firstPage").style.display = "none";
-        }else{
-        alert("Ingrese su nombre");
-        return;
-        }
-
-    // Si la tarjeta es válida, se visualizara el "secondPage"... Si la tarjeta es inválida se visualizara el "thirdPage"
-       if (isValid === true){
-        document.getElementById("secondPage").style.display = "block";
-        document.getElementById("cardInformation").innerHTML += `<p class="letters">${maskify}</p>`
-        document.getElementById("cardInformation").innerHTML += `<p class="letters">${name+ " tu tarjeta es válida"}</p>`
-
-             } else {
-            document.getElementById("thirdPage").style.display = "block";
-            document.getElementById("secondInformation").innerHTML += `<p class="letters">${maskify}</p>`
-            document.getElementById("secondInformation").innerHTML += `<p class="letters">${name+ " tu tarjeta es inválida"}</p>`
-            } 
-
-
-})
-const btnHouse= document.getElementById("house")
-
-btnHouse.addEventListener('click', ()=>{
-
-
-    // Al presionar el boton "house" se devolvera la pagina de inicio
-    document.getElementById("secondPage").style.display = "none";
-    document.getElementById("firstPage").style.display = "block";
-
-
-})
-
-const btnHouseTwo= document.getElementById("houseTwo")
-
-btnHouseTwo.addEventListener('click', ()=>{
-
-    // Al presionar el boton "home" se devolvera la pagina de inicio
-    document.getElementById("thirdPage").style.display = "none";
-    document.getElementById("firstPage").style.display = "block";
-
-
-})
-
+    Swal.fire({
+        title: isValid ? "Tarjeta válida" : "Oops...",
+        html: `
+            <p>${maskify}</p>
+            <p>${name} tu tarjeta es ${isValid ? 'válida' : 'inválida'}</p>
+        `,
+        icon: isValid ? "success" : "error"
+    });
+    if (isValid) {
+        nameInput.value = '';
+        numberInput.value = '';
+    }
+});
 
